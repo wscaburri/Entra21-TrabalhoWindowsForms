6c4b75b248d65ff52e8adbf67a591cdf1589e802
Cristyan Alex <cristyanalexandrino.od@gmail.com>
2022-06-29 15:18:47 -0300

    Criado classe Responsavel

---
+++ Entra21-TrabalhoWindowsForms/Responsavel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21_TrabalhoWindowsForms
+{
+    internal class Responsavel
+    {
+        public int Codigo;
+        public string NomeCompleto;
+        public DateTime DataNascimento;
+        public double Cpf;
+        public string Rg;
+        public double Cep;
+        public string Endereco;
+        public double NumeroResidencia;
+        public string Complemento;
+        public string Cidade;
+        public string Bairro;
+        public string LocalDeTrabalho;
+        public DateTime DataDeCadastro;
+        public string Telefone;
+        public string Email;
+
+    }
+}
