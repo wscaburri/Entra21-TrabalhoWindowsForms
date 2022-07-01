288d654c4e3ef6ad00a5dc4e15c4e44c72a0f7e3
Wellington Scaburri <wscaburri@gmail.com>
2022-07-01 16:30:50 -0300

    Registro Geral

--- Entra21-TrabalhoWindowsForms/RegistroGeralForm.cs
+++ Entra21-TrabalhoWindowsForms/RegistroGeralForm.cs
@@ -1,53 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace Entra21_TrabalhoWindowsForms
+﻿namespace Entra21_TrabalhoWindowsForms
 {
     public partial class RegistroGeralForm : Form
     {
         private AgendamentoConsultaForm agendamentoConsultaForm;
-        private AgendamentoConsultaServico 
+        private AgendamentoConsultaServico agendamentoConsultaServico;
+
         public RegistroGeralForm()
         {
             InitializeComponent();
 
             agendamentoConsultaForm = new AgendamentoConsultaForm();
-        }
-
-        private void PreencherDataGridViewComAgendamentos()
-        {
-            var agendamentos = agendamentoConsultaServico.ObterTodos();
-
-            dataGridViewAgendamentoConsulta.Rows.Clear();
-
-            for (var i = 0; i < agendamentos.Count; i++)
-            {
-                var agendamento = agendamentos[i];
-
-                dataGridViewAgendamentoConsulta.Rows.Add(new object[]
-                {
-                        agendamento.Codigo,
-                        agendamento.DataConsulta.ToString("dd/MM/yyyy"),
-                        agendamento.HoraConsulta,
-                        agendamento.NomeResponsavel,
-                        agendamento.NomePet,
-                        agendamento.NomeVeterinario
-                });
-            }
-
-            dataGridViewAgendamentoConsulta.ClearSelection();
+            agendamentoConsultaServico = new AgendamentoConsultaServico();
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            
         }
     }
 }
