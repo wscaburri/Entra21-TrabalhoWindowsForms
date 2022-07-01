56f1f591497be68539871aa1826eb70d186c4c20
Wellington Scaburri <wscaburri@gmail.com>
2022-07-01 15:47:22 -0300

    ...

--- Entra21-TrabalhoWindowsForms/RegistroGeralForm.Designer.cs
+++ Entra21-TrabalhoWindowsForms/RegistroGeralForm.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ColumnDataConsulta = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnHoraConsulta = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnNomeResponsável = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnNomePet = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -37,6 +41,11 @@
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnDataConsulta,
+            this.ColumnHoraConsulta,
+            this.ColumnNomeResponsável,
+            this.ColumnNomePet});
             this.dataGridView1.Location = new System.Drawing.Point(29, 32);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
@@ -44,15 +53,48 @@
             this.dataGridView1.RowTemplate.Height = 29;
             this.dataGridView1.Size = new System.Drawing.Size(780, 663);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
-            // RegistroGeral
+            // ColumnDataConsulta
+            // 
+            this.ColumnDataConsulta.HeaderText = "Data da Consulta:";
+            this.ColumnDataConsulta.MinimumWidth = 6;
+            this.ColumnDataConsulta.Name = "ColumnDataConsulta";
+            this.ColumnDataConsulta.ReadOnly = true;
+            this.ColumnDataConsulta.Width = 125;
+            // 
+            // ColumnHoraConsulta
+            // 
+            this.ColumnHoraConsulta.HeaderText = "Hora da Consulta:";
+            this.ColumnHoraConsulta.MinimumWidth = 6;
+            this.ColumnHoraConsulta.Name = "ColumnHoraConsulta";
+            this.ColumnHoraConsulta.ReadOnly = true;
+            this.ColumnHoraConsulta.Width = 125;
+            // 
+            // ColumnNomeResponsável
+            // 
+            this.ColumnNomeResponsável.HeaderText = "Nome do Responsável";
+            this.ColumnNomeResponsável.MinimumWidth = 6;
+            this.ColumnNomeResponsável.Name = "ColumnNomeResponsável";
+            this.ColumnNomeResponsável.ReadOnly = true;
+            this.ColumnNomeResponsável.Width = 125;
+            // 
+            // ColumnNomePet
+            // 
+            this.ColumnNomePet.HeaderText = "Nome do Pet:";
+            this.ColumnNomePet.MinimumWidth = 6;
+            this.ColumnNomePet.Name = "ColumnNomePet";
+            this.ColumnNomePet.ReadOnly = true;
+            this.ColumnNomePet.Width = 125;
+            // 
+            // RegistroGeralForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(846, 724);
             this.Controls.Add(this.dataGridView1);
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.Name = "RegistroGeral";
+            this.Name = "RegistroGeralForm";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
             this.Text = "RegistroGeral";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
@@ -64,5 +106,9 @@
         #endregion
 
         private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn ColumnDataConsulta;
+        private DataGridViewTextBoxColumn ColumnHoraConsulta;
+        private DataGridViewTextBoxColumn ColumnNomeResponsável;
+        private DataGridViewTextBoxColumn ColumnNomePet;
     }
 }
--- Entra21-TrabalhoWindowsForms/RegistroGeralForm.cs
+++ Entra21-TrabalhoWindowsForms/RegistroGeralForm.cs
@@ -12,9 +12,42 @@ namespace Entra21_TrabalhoWindowsForms
 {
     public partial class RegistroGeralForm : Form
     {
+        private AgendamentoConsultaForm agendamentoConsultaForm;
+        private AgendamentoConsultaServico 
         public RegistroGeralForm()
         {
             InitializeComponent();
+
+            agendamentoConsultaForm = new AgendamentoConsultaForm();
+        }
+
+        private void PreencherDataGridViewComAgendamentos()
+        {
+            var agendamentos = agendamentoConsultaServico.ObterTodos();
+
+            dataGridViewAgendamentoConsulta.Rows.Clear();
+
+            for (var i = 0; i < agendamentos.Count; i++)
+            {
+                var agendamento = agendamentos[i];
+
+                dataGridViewAgendamentoConsulta.Rows.Add(new object[]
+                {
+                        agendamento.Codigo,
+                        agendamento.DataConsulta.ToString("dd/MM/yyyy"),
+                        agendamento.HoraConsulta,
+                        agendamento.NomeResponsavel,
+                        agendamento.NomePet,
+                        agendamento.NomeVeterinario
+                });
+            }
+
+            dataGridViewAgendamentoConsulta.ClearSelection();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
         }
     }
 }
