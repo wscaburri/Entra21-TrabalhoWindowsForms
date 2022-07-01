89b6c7d83260ad21c2a1d56cb8ed341de502e959
Juliana <julianaforbicii>
2022-07-01 16:22:08 -0300

    Tela Menu feita.

--- Entra21-TrabalhoWindowsForms/Menu.Designer.cs
+++ Entra21-TrabalhoWindowsForms/Menu.Designer.cs
@@ -3,12 +3,12 @@
     partial class Menu
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,128 +23,122 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.buttonRegistroGeral = new System.Windows.Forms.Button();
-            this.buttonCadastroReponsavel = new System.Windows.Forms.Button();
-            this.buttonCadastroPet = new System.Windows.Forms.Button();
-            this.buttonCadsatroClinicoGeral = new System.Windows.Forms.Button();
-            this.buttonMarcarHorario = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
+            this.buttonCadastroAnimal = new System.Windows.Forms.Button();
             this.buttonCadastroRemedios = new System.Windows.Forms.Button();
-            this.labelBoasVindas = new System.Windows.Forms.Label();
+            this.buttonCadastroCliente = new System.Windows.Forms.Button();
+            this.buttonAgendamentoConsulta = new System.Windows.Forms.Button();
+            this.buttonCadastroVeterinario = new System.Windows.Forms.Button();
+            this.buttonRegistroGeral = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // buttonRegistroGeral
+            // buttonCadastroAnimal
             // 
-            this.buttonRegistroGeral.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.buttonRegistroGeral.Font = new System.Drawing.Font("Perpetua Titling MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.buttonRegistroGeral.Location = new System.Drawing.Point(12, 259);
-            this.buttonRegistroGeral.Name = "buttonRegistroGeral";
-            this.buttonRegistroGeral.Size = new System.Drawing.Size(203, 298);
-            this.buttonRegistroGeral.TabIndex = 0;
-            this.buttonRegistroGeral.Text = "Registro Geral de Clientes";
-            this.buttonRegistroGeral.UseVisualStyleBackColor = true;
-            this.buttonRegistroGeral.Click += new System.EventHandler(this.buttonRegistroGeral_Click);
-            // 
-            // buttonCadastroReponsavel
-            // 
-            this.buttonCadastroReponsavel.Font = new System.Drawing.Font("Perpetua Titling MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.buttonCadastroReponsavel.Location = new System.Drawing.Point(277, 256);
-            this.buttonCadastroReponsavel.Name = "buttonCadastroReponsavel";
-            this.buttonCadastroReponsavel.Size = new System.Drawing.Size(203, 301);
-            this.buttonCadastroReponsavel.TabIndex = 1;
-            this.buttonCadastroReponsavel.Text = "Cadastro Responsável";
-            this.buttonCadastroReponsavel.UseVisualStyleBackColor = true;
-            this.buttonCadastroReponsavel.Click += new System.EventHandler(this.buttonCadastroReponsavel_Click);
-            // 
-            // buttonCadastroPet
-            // 
-            this.buttonCadastroPet.Font = new System.Drawing.Font("Perpetua Titling MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.buttonCadastroPet.Location = new System.Drawing.Point(793, 259);
-            this.buttonCadastroPet.Name = "buttonCadastroPet";
-            this.buttonCadastroPet.Size = new System.Drawing.Size(242, 298);
-            this.buttonCadastroPet.TabIndex = 2;
-            this.buttonCadastroPet.Text = "Cadastro Pet";
-            this.buttonCadastroPet.UseVisualStyleBackColor = true;
-            // 
-            // buttonCadsatroClinicoGeral
-            // 
-            this.buttonCadsatroClinicoGeral.Font = new System.Drawing.Font("Perpetua Titling MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.buttonCadsatroClinicoGeral.Location = new System.Drawing.Point(538, 259);
-            this.buttonCadsatroClinicoGeral.Name = "buttonCadsatroClinicoGeral";
-            this.buttonCadsatroClinicoGeral.Size = new System.Drawing.Size(224, 298);
-            this.buttonCadsatroClinicoGeral.TabIndex = 3;
-            this.buttonCadsatroClinicoGeral.Text = "Cadastro  Clinico Geral";
-            this.buttonCadsatroClinicoGeral.UseVisualStyleBackColor = true;
-            // 
-            // buttonMarcarHorario
-            // 
-            this.buttonMarcarHorario.Font = new System.Drawing.Font("Perpetua Titling MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.buttonMarcarHorario.Location = new System.Drawing.Point(1057, 259);
-            this.buttonMarcarHorario.Name = "buttonMarcarHorario";
-            this.buttonMarcarHorario.Size = new System.Drawing.Size(231, 298);
-            this.buttonMarcarHorario.TabIndex = 4;
-            this.buttonMarcarHorario.Text = "Marcar Horário de Consulta";
-            this.buttonMarcarHorario.UseVisualStyleBackColor = true;
+            this.buttonCadastroAnimal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.buttonCadastroAnimal.Font = new System.Drawing.Font("Yu Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonCadastroAnimal.Location = new System.Drawing.Point(146, 327);
+            this.buttonCadastroAnimal.Name = "buttonCadastroAnimal";
+            this.buttonCadastroAnimal.Size = new System.Drawing.Size(142, 111);
+            this.buttonCadastroAnimal.TabIndex = 0;
+            this.buttonCadastroAnimal.Text = "Cadastro Animal";
+            this.buttonCadastroAnimal.UseVisualStyleBackColor = false;
+            this.buttonCadastroAnimal.Click += new System.EventHandler(this.buttonCadastroAnimal_Click);
             // 
             // buttonCadastroRemedios
             // 
-            this.buttonCadastroRemedios.Font = new System.Drawing.Font("Perpetua Titling MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.buttonCadastroRemedios.Location = new System.Drawing.Point(1521, 259);
+            this.buttonCadastroRemedios.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.buttonCadastroRemedios.Font = new System.Drawing.Font("Yu Gothic", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonCadastroRemedios.Location = new System.Drawing.Point(324, 327);
             this.buttonCadastroRemedios.Name = "buttonCadastroRemedios";
-            this.buttonCadastroRemedios.Size = new System.Drawing.Size(280, 386);
-            this.buttonCadastroRemedios.TabIndex = 5;
-            this.buttonCadastroRemedios.Text = "Cadastro de Remédios/Vacinas";
-            this.buttonCadastroRemedios.UseVisualStyleBackColor = true;
+            this.buttonCadastroRemedios.Size = new System.Drawing.Size(142, 111);
+            this.buttonCadastroRemedios.TabIndex = 1;
+            this.buttonCadastroRemedios.Text = "Cadastro Medicamentos";
+            this.buttonCadastroRemedios.UseVisualStyleBackColor = false;
             this.buttonCadastroRemedios.Click += new System.EventHandler(this.buttonCadastroRemedios_Click);
             // 
-            // labelBoasVindas
+            // buttonCadastroCliente
             // 
-            this.labelBoasVindas.AutoSize = true;
-            this.labelBoasVindas.BackColor = System.Drawing.SystemColors.Info;
-            this.labelBoasVindas.Font = new System.Drawing.Font("Rockwell", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.labelBoasVindas.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.labelBoasVindas.LiveSetting = System.Windows.Forms.Automation.AutomationLiveSetting.Assertive;
-            this.labelBoasVindas.Location = new System.Drawing.Point(442, 29);
-            this.labelBoasVindas.Name = "labelBoasVindas";
-            this.labelBoasVindas.Size = new System.Drawing.Size(441, 54);
-            this.labelBoasVindas.TabIndex = 6;
-            this.labelBoasVindas.Text = "Clinica Veterinaria";
+            this.buttonCadastroCliente.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.buttonCadastroCliente.Font = new System.Drawing.Font("Yu Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonCadastroCliente.ForeColor = System.Drawing.Color.Black;
+            this.buttonCadastroCliente.Location = new System.Drawing.Point(146, 179);
+            this.buttonCadastroCliente.Name = "buttonCadastroCliente";
+            this.buttonCadastroCliente.Size = new System.Drawing.Size(142, 111);
+            this.buttonCadastroCliente.TabIndex = 2;
+            this.buttonCadastroCliente.Text = "Cadastro Cliente";
+            this.buttonCadastroCliente.UseVisualStyleBackColor = false;
+            this.buttonCadastroCliente.Click += new System.EventHandler(this.buttonCadastroCliente_Click);
+            // 
+            // buttonAgendamentoConsulta
+            // 
+            this.buttonAgendamentoConsulta.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.buttonAgendamentoConsulta.Font = new System.Drawing.Font("Yu Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonAgendamentoConsulta.Location = new System.Drawing.Point(324, 179);
+            this.buttonAgendamentoConsulta.Name = "buttonAgendamentoConsulta";
+            this.buttonAgendamentoConsulta.Size = new System.Drawing.Size(142, 111);
+            this.buttonAgendamentoConsulta.TabIndex = 3;
+            this.buttonAgendamentoConsulta.Text = "Agendamento de Consulta";
+            this.buttonAgendamentoConsulta.UseVisualStyleBackColor = false;
+            this.buttonAgendamentoConsulta.Click += new System.EventHandler(this.buttonAgendamentoConsulta_Click);
+            // 
+            // buttonCadastroVeterinario
+            // 
+            this.buttonCadastroVeterinario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.buttonCadastroVeterinario.Font = new System.Drawing.Font("Yu Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonCadastroVeterinario.Location = new System.Drawing.Point(511, 327);
+            this.buttonCadastroVeterinario.Name = "buttonCadastroVeterinario";
+            this.buttonCadastroVeterinario.Size = new System.Drawing.Size(142, 111);
+            this.buttonCadastroVeterinario.TabIndex = 5;
+            this.buttonCadastroVeterinario.Text = "Cadastro Veterinário";
+            this.buttonCadastroVeterinario.UseVisualStyleBackColor = false;
+            this.buttonCadastroVeterinario.Click += new System.EventHandler(this.buttonCadastroVeterinario_Click);
+            // 
+            // buttonRegistroGeral
+            // 
+            this.buttonRegistroGeral.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.buttonRegistroGeral.Font = new System.Drawing.Font("Yu Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonRegistroGeral.Location = new System.Drawing.Point(511, 179);
+            this.buttonRegistroGeral.Name = "buttonRegistroGeral";
+            this.buttonRegistroGeral.Size = new System.Drawing.Size(142, 111);
+            this.buttonRegistroGeral.TabIndex = 6;
+            this.buttonRegistroGeral.Text = "Registro Geral";
+            this.buttonRegistroGeral.UseVisualStyleBackColor = false;
+            this.buttonRegistroGeral.Click += new System.EventHandler(this.buttonRegistroGeral_Click);
             // 
             // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(1326, 625);
-            this.Controls.Add(this.labelBoasVindas);
-            this.Controls.Add(this.buttonCadastroRemedios);
-            this.Controls.Add(this.buttonMarcarHorario);
-            this.Controls.Add(this.buttonCadsatroClinicoGeral);
-            this.Controls.Add(this.buttonCadastroPet);
-            this.Controls.Add(this.buttonCadastroReponsavel);
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.buttonRegistroGeral);
+            this.Controls.Add(this.buttonCadastroVeterinario);
+            this.Controls.Add(this.buttonAgendamentoConsulta);
+            this.Controls.Add(this.buttonCadastroCliente);
+            this.Controls.Add(this.buttonCadastroRemedios);
+            this.Controls.Add(this.buttonCadastroAnimal);
             this.Name = "Menu";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Form1";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Text = "Menu";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private Button buttonRegistroGeral;
-        private Button buttonCadastroReponsavel;
-        private Button buttonCadastroPet;
-        private Button buttonCadsatroClinicoGeral;
-        private Button buttonMarcarHorario;
+        private Button buttonCadastroAnimal;
         private Button buttonCadastroRemedios;
-        private Label labelBoasVindas;
+        private Button buttonCadastroCliente;
+        private Button buttonAgendamentoConsulta;
+        private Button buttonCadastroVeterinario;
+        private Button buttonRegistroGeral;
     }
 }
